78676ce5e38eae25ea40d206689a3794597fa5bd
yasasri <yasasri999@gmail.com>
2024-06-24 16:39:00 +0530

    displaying ball wise result Adding a form such that only on event handling scores are displayed and stored in

---
+++ react.js
@@ -0,0 +1,77 @@
+let score = 0;
+let wicket = 0;
+let ballWiseRes = [];
+let hit = 0;
+
+function addScore(num) {
+  hit = num;
+  rootElement.render(<App />);
+  console.log(hit);
+}
+function addWicket(num) {
+  hit = "w";
+  rootElement.render(<App />);
+}
+
+const ScoreButtons = () => (
+  <div>
+    <button onClick={() => addScore(0)}>0</button>
+    <button onClick={() => addScore(1)}>1</button>
+    <button onClick={() => addScore(2)}>2</button>
+    <button onClick={() => addScore(3)}>3</button>
+    <button onClick={() => addScore(4)}>4</button>
+    <button onClick={() => addScore(5)}>5</button>
+    <button onClick={() => addScore(6)}>6</button>
+    <button onClick={addWicket}>wicket</button>
+  </div>
+);
+
+const Results = () => (
+  <div>
+    {ballWiseRes.map((res, index) => (
+      <>
+        {index % 6 === 0 ? <br /> : null}
+        <span key={index}>{res === 0 ? <strong>.</strong> : res}</span>&nbsp;
+      </>
+    ))}
+  </div>
+);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (hit == "w") {
+    wicket += 1;
+  } else {
+    score += hit;
+  }
+  ballWiseRes.unshift(hit);
+  console.log(ballWiseRes);
+  rootElement.render(<App />);
+}
+
+const Form = () => (
+  <form onSubmit={handleSubmit}>
+    <input value={hit} />
+    <input />
+    <button>Submit</button>
+  </form>
+);
+const App = () => (
+  <>
+    <h1>SCORE KEEPER</h1>
+    <h2>
+      SCORE : {score}/{wicket}
+    </h2>
+    <ScoreButtons />
+    <br />
+
+    <Form />
+    <hr />
+    {ballWiseRes.map((res, index) => (
+      <p key={index}>{res}</p>
+    ))}
+  </>
+);
+
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(<App />);
